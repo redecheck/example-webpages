90d3d4d9fe758367954eb539943e51b5dee33249
Thomas Walsh <twalsh1@sheffield.ac.uk>
2017-01-24 12:03:16 +0000

    Consumer Reports okay?

---
+++ resizer.js
@@ -0,0 +1,3 @@
+<script type="text/javascript">
+var newwindow=window.open("file:///Users/thomaswalsh/Documents/PhD/Resources/fault-examples/RainyMood/index.html",'test','width=320,height=1024,top=0,left=0');
+</script>

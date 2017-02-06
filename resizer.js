e7980ee46b023d1d86b24a6f4e574243f6ef64e4
Thomas Walsh <twalsh1@sheffield.ac.uk>
2017-02-06 14:39:58 +0000

    Started clean up of repository

--- resizer.js
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript">
-var newwindow=window.open("file:///Users/thomaswalsh/Documents/PhD/Resources/fault-examples/RainyMood/index.html",'test','width=320,height=1024,top=0,left=0');
-</script>
